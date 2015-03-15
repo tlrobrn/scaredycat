c4d5527dcd9f04fef6ea944b187db580fa09f956
Taylor O'Brien <obrien.taylorm@gmail.com>
2015-03-15 17:41:40 -0500

    initial setup

---
+++ desktop/src/com/gr8bitsoftware/scaredycat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gr8bitsoftware.scaredycat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gr8bitsoftware.scaredycat.ScaredyCatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ScaredyCatGame(), config);
+	}
+}
