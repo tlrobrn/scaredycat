a3baa0ee36a0e94a34e720c274862087da5eee46
Taylor O'Brien <obrien.taylorm@gmail.com>
2015-03-15 19:18:51 -0500

    Simple Game extension with orthographic camera.

--- core/src/com/gr8bitsoftware/scaredycat/ScaredyCatGame.java
+++ core/src/com/gr8bitsoftware/scaredycat/ScaredyCatGame.java
@@ -1,27 +1,24 @@
 package com.gr8bitsoftware.scaredycat;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
-public class ScaredyCatGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ScaredyCatGame extends Game {
+	public static OrthographicCamera camera;
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		this.setScreen(MainMenuScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+
 	}
 }
